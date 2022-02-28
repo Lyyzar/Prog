2415cc4dd586d080d02225d98a3433781b2a4189
Kristóf Balogh <baloghkristof1000@gmail.com>
2022-02-28 21:10:03 +0100

    Almost done

--- Programozás/ch14/ch14drill.cpp
+++ Programozás/ch14/ch14drill.cpp
@@ -3,7 +3,9 @@
 struct B1{
 
 		virtual void vf() 	{cout << "B1::vf()\n";}
+		virtual void pvf()	{cout << "B1::pvf()\n";}
 		void f()			{cout << "B1::f()\n";}
+
 	};
 	
 struct D1:B1{
@@ -12,23 +14,60 @@ struct D1:B1{
 		void f()			{cout << "D1::f()\n";}
 };
 
+struct D2:D1{
 
+		void pvf() override {cout << "D2::pvf()\n";}
+};
 
+struct B2{
 
+		virtual void pvf()	{cout<<"B2::pvf()\n";}
+};
+
+struct D21:B2{
+
+		string e="something";
+		void pvf() override	{cout <<e<<endl;}
+};
+
+struct D22:B2{
+
+		int f=10;
+		void pvf() override	{cout <<f<<endl;}
+};
+
+void f(B2& g){
+
+		g.pvf();
+}
 
 
 int main(){
 
 
 	B1 a;
-	D1 b;
-
-
 	a.vf();
 	a.f();
 
-	b.vf();
+	D1 c;
+	c.vf();
+	c.f();
+
+/*
+	B1& b;
 	b.f();
+	b.vf();
+*/
+	D2 d;
+	d.f();
+	d.vf();
+	d.pvf();
+
+	D21	h;
+	D22 i;
+
+	f(h);
+	f(i);
 
 
 
