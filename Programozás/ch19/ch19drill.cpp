0a21d42b384e8d2dc6f0233c90f94fe6320550c0
Kristóf Balogh <baloghkristof1000@gmail.com>
2022-03-27 11:15:31 +0200

    Beginning

---
+++ Programozás/ch19/ch19drill.cpp
@@ -0,0 +1,33 @@
+#include "std_lib_facilities.h"
+
+
+template<typename T> 
+
+struct S {
+
+private: T val;
+
+public: 
+void fill(T&,s){
+	
+	for (int i=0;i<=n;i++)
+		S[i]=s;
+	
+	
+	
+	}
+
+
+}
+
+int main()
+{
+
+
+
+
+
+
+
+return 0;
+}
