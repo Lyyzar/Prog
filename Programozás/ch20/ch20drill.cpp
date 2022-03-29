19a6809da0d7e311e9a3a227db38d5eb1591ff2f
Kristóf Balogh <baloghkristof1000@gmail.com>
2022-03-29 14:36:01 +0200

    Beginning

---
+++ Programozás/ch20/ch20drill.cpp
@@ -0,0 +1,16 @@
+#include "std_lib_facilities.h"
+
+int main(){
+
+	int A[10] = {0,1,2,3,4,5,6,7,8,9};
+	vector<int> B{0,1,2,3,4,5,6,7,8,9};
+	list<int> C;
+		C.insert(0,1,2,3,4,5,6,7,8,9);
+
+
+
+
+
+
+return  0;
+}
