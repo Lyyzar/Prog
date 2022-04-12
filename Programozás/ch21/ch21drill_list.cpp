f1ad58c0df070ee5eea96e0d4ca795d4a4378c92
Kristóf Balogh <baloghkristof1000@gmail.com>
2022-04-12 15:23:05 +0200

    Done

--- Programozás/ch21/ch21drill_list.cpp
+++ Programozás/ch21/ch21drill_list.cpp
@@ -38,50 +38,48 @@ struct Compare_by_value{
 
 void print_lis(list<Item>& vc)
 {
-for(int i=0;i<10;++i)
-	cout<<vc[i];
+for(auto& item:vc)
+cout<< item<<endl;
 
 }
 
 
 int main(){
-vector<Item> vi(10);
+list<Item> vi(10);
 ifstream ifile {"data.txt"};
 
 
-for(int i=0;i<10;++i)
-	{
-		ifile>>vi[i];
-	}
+for(auto& i : vi)
+ifile>>i.name>>i.iid>>i.value;
 	ifile.close();
 
 cout<<"Before sort by name: "<<endl;
-print_vec(vi);
+print_lis(vi);
 
-sort(vi.begin(),vi.end(),Compare_by_name());
+vi.sort([] (Item a,Item b){return a.name<b.name;});
 
 cout<<"After sort by name: "<<endl;
-print_vec(vi);
+print_lis(vi);
 
-sort(vi.begin(),vi.end(),Compare_by_iid());
+vi.sort([] (Item a,Item b){return a.iid<b.iid;});
 
 cout<<"After sort by iid: "<<endl;
-print_vec(vi);
+print_lis(vi);
 
-sort(vi.begin(),vi.end(),Compare_by_value());
+vi.sort([] (Item a,Item b){return a.value>b.value;});
 
 cout<<"After sort by value: "<<endl;
-print_vec(vi);
+print_lis(vi);
 
 vi.insert(vi.begin(),Item{"horse shoe",99,12.34});
 vi.insert(vi.begin(),Item{"Canon S400",9988,499.95});
 cout<<"After insert 2 Items: "<<endl;
-print_vec(vi);
+print_lis(vi);
 
 vi.erase(find_if(vi.begin(),vi.end(), [] (Item i) {return i.name=="Toothbrush";}));
 vi.erase(find_if(vi.begin(),vi.end(), [] (Item i) {return i.name=="Lamp";}));
 cout<<"After erased 2 Items: "<<endl;
-print_vec(vi);
+print_lis(vi);
 
 
 	return 0;
