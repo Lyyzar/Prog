9da80f34b73726d011fd6c951f37d2eec3cee2a7
Kristóf Balogh <baloghkristof1000@gmail.com>
2022-04-21 11:22:26 +0200

    Done

--- Programozás/ch21/ch21drill_map.cpp
+++ Programozás/ch21/ch21drill_map.cpp
@@ -1,34 +1,57 @@
 #include "std_lib_facilities.h"
 
-/*
-print_map(map<string,int>& ma)
+template<typename T, typename F>
+void print_map(map<T,F>& ma)
 {
-
-
-
-
-
+	for(const auto p : ma)
+		cout <<p.first<< " " <<p.second<<endl;
 }
 
-ostream& operator<<(ostream& os, map<string,int>)
-*/
+template<typename T, typename F>
+void read_cin(map<T,F>& l)
+{
+	pair<T,F> par;
+	int a=-1;
+
+	while(cin>>par.first>>par.second)
+		{
+			++a;
+			if(a<10) l[par.first]=par.second;
+				else break;
+		}
+}
+
+
 int main(){
 
 
 	map<string,int> msi;
-	string a;
-	int b;
 	
-	while(cin>>a>>b && b!='q')
-	{
-	msi[a]=b;
-	}
-	for(int i=0;i<msi.size();++i)
-	cout<<msi.first<<msi.second<<endl;
+	ifstream ifile("data.txt");
+	for(int i=0;i<10;++i)
+		{
+			string key;
+			int k;
+			double value;
+			ifile>>key>>k>>value;
+			msi[key]=k;
+		} 
+	ifile.close();
 
+	print_map(msi);
+	msi.erase(msi.begin(),msi.end());
 
+	read_cin(msi);
+	cout<<"New map: "<<endl;
+	print_map(msi);
+	cout<<endl;
+	cout<<"Accumulate: ";
+	cout<< accumulate(msi.begin(),msi.end(), 0, [] (int i, auto p) {return i+p.second;})<<endl;
 
-
+map<int,string> mis;
+for(const auto& i : msi)
+mis[i.second]=i.first;
+print_map(mis);
 
 
 return 0;
