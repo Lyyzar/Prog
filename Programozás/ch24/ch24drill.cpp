e20b226b5572ea5c9a9ca309e42e9006dbe2e9c1
Kristóf Balogh <baloghkristof1000@gmail.com>
2022-04-21 11:37:51 +0200

    Done?

---
+++ Programozás/ch24/ch24drill.cpp
@@ -0,0 +1,30 @@
+#include "std_lib_facilities.h"
+#include "Matrix.h"
+
+using namespace std;
+
+int main(){
+	
+	cout<<"Size of char: "<< sizeof(char)<<endl;
+	cout<<"Size of short: "<< sizeof(short)<<endl;
+	cout<<"Size of int: "<< sizeof(int)<<endl;
+	cout<<"Size of long: "<< sizeof(long)<<endl;
+	cout<<"Size of float: "<< sizeof(float)<<endl;
+	cout<<"Size of double: "<< sizeof(double)<<endl;
+	cout<<"Size of int*: "<< sizeof(int*)<<endl;
+	cout<<"Size of double*: "<< sizeof(double*)<<endl;
+
+	Numeric_lib::Matrix<int> a(10);
+	Numeric_lib::Matrix<int> b(100);
+	Numeric_lib::Matrix<double> c(10);
+	Numeric_lib::Matrix<int,2> d(10,10);
+	Numeric_lib::Matrix<int,3> e(10,10,10);
+
+
+	cout<<"Size of a: "<< sizeof(a)<<endl;
+	cout<<"Size of b: "<< sizeof(b)<<endl;
+	cout<<"Size of c: "<< sizeof(c)<<endl;
+	cout<<"Size of d: "<< sizeof(d)<<endl;
+	cout<<"Size of e: "<< sizeof(e)<<endl;
+	return 0;
+}
