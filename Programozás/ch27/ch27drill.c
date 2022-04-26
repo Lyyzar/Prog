707469f1a478c3f66b6cb5489f01348a65571f61
Kristóf Balogh <baloghkristof1000@gmail.com>
2022-04-26 14:16:18 +0200

    Almost

---
+++ Programozás/ch27/ch27drill.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void my_print(char* p,int x)
+{
+	printf(,p,x)
+
+
+}
+
+
+int main(){
+
+char* h="Hello";
+char* w="World!";
+printf("%s %s",h,w);
+
+char* p="foo";
+int x=7;
+
+
+
+
+
+return 0;
+}
